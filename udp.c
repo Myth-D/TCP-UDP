4e3d10ec998873af835623c0bc2061fabf537ffa
Myth <Dragon4453@163.com>
2023-09-12 13:51:21 +0800

    修改：     udp.c

--- udp.c
+++ udp.c
@@ -11,10 +11,10 @@ void *rcv_msg(void *arg)
 
 /**
  * @brief   发送消息模块
- * @param   arg 无
+ * @param   IP 对方的IP的地址
  * @return  无
  */
-void *snd_msg(void *arg)
+void *snd_msg(void *IP)
 {
 }
 
@@ -25,8 +25,18 @@ void *snd_msg(void *arg)
  */
 int main(int argc, char const *argv[])
 {
+    if (argc != 2)
+    {
+        printf("请指定对方的IP地址!\n");
+        exit(EXIT_FAILURE);
+    }
+
     pthread_t rcv_tid;
     pthread_t snd_tid;
 
+    pthread_create(rcv_tid, NULL, rcv_msg, NULL);
+    pthread_create(snd_tid, NULL, snd_msg, (void *)argv[1]);
+
+    pthread_exit(NULL);
     return 0;
 }
