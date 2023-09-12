c0ad5f90355e2371b87fb16b5a837bb30feb26b1
Myth <Dragon4453@163.com>
2023-09-12 10:01:15 +0800

    更新：     head.h

---
+++ head.h
@@ -0,0 +1,22 @@
+#ifndef _HEAD_H
+#define _HEAD_H 1
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+
+#include <pthread.h>
+
+#endif /* _HEAD_H */
