2c330438343d98951112c50711f6b77fdc769632
Myth <Dragon4453@163.com>
2023-09-12 10:03:32 +0800

    修改：     udp.c

---
+++ udp.c
@@ -0,0 +1,17 @@
+#include "head.h"
+
+void *rcv_msg(void *arg)
+{
+}
+
+void *snd_msg(void *arg)
+{
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t rcv_tid;
+    pthread_t snd_tid;
+
+    return 0;
+}
