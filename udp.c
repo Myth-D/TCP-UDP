7d8d5f1df71e90312588f22b8b1cc55d5874e659
Myth <Dragon4453@163.com>
2023-09-12 11:18:07 +0800

    修改：     udp.c

--- udp.c
+++ udp.c
@@ -1,13 +1,28 @@
 #include "head.h"
 
+/**
+ * @brief   接收信息模块
+ * @param   arg 无
+ * @return  无
+ */
 void *rcv_msg(void *arg)
 {
 }
 
+/**
+ * @brief   发送消息模块
+ * @param   arg 无
+ * @return  无
+ */
 void *snd_msg(void *arg)
 {
 }
 
+/**
+ * @brief   主控程序
+ * @param   argv[1] 对方的IP地址
+ * @return  无
+ */
 int main(int argc, char const *argv[])
 {
     pthread_t rcv_tid;
